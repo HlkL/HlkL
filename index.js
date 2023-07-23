11b800223b517e9bc46e895b2988426c601a3616
HlkL <83001164+HlkL@users.noreply.github.com>
2023-07-23 16:24:49 +0800

    Update index.js
    fix:

--- index.js
+++ index.js
@@ -14,7 +14,6 @@ function generateProgressBar() {
 }
 
 const readme = `\
-### [![Typing SVG](https://readme-typing-svg.demolab.com?font=Fira+Code&size=13&pause=1000&color=626DF7&background=747AFF00&width=435&lines=%E5%8A%AA%E5%8A%9B%E6%98%AF%E4%BC%9A%E4%B8%8A%E7%98%BE%E7%9A%84%EF%BC%8C%E7%89%B9%E5%88%AB%E6%98%AF%E5%B0%9D%E5%88%B0%E7%94%9C%E5%A4%B4%E4%B9%8B%E5%90%8E)](https://git.io/typing-svg)
 ⏳ Year progress ${progressBarOfThisYear} ${(progressOfThisYear * 100).toFixed(2)} %
 ---
 ⏰ Updated on ${new Date().toUTCString()}
